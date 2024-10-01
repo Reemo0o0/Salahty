7080e0e94c53fa63b68101bb77e96e33d8de783c
Reem Alotaibi <Reemo0o_20015@hotmail.com>
2024-10-01 15:54:33 +0300

    Initial Commit

---
+++ SalahtyTests/SalahtyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalahtyTests.swift
+//  SalahtyTests
+//
+//  Created by Reem Alotaibi on 28/03/1446 AH.
+//
+
+import Testing
+@testable import Salahty
+
+struct SalahtyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
