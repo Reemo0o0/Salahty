e595d9181031c66286b06ba4ad248fd4d8c7dc69
shuruq alshammari <shuruqalsh@gmail.com>
2024-10-08 09:23:51 +0300

    Full app  & Audio  (Without UserDefaults )

--- Salahty/AppDelegate.swift
+++ Salahty/AppDelegate.swift
@@ -6,35 +6,48 @@
 //
 
 import UIKit
+import AVFoundation // استيراد المكتبة
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var audioPlayer: AVAudioPlayer? // متغير مشغل الصوت
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // تشغيل الصوت عند فتح التطبيق
+        playSound()
         return true
     }
 
+    func playSound() {
+        // تحديد مكان ملف الصوت
+        if let soundURL = Bundle.main.url(forResource: "BackgrpundOst", withExtension: "mp3") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer?.volume = 0.3 // ضبط مستوى الصوت (0.5 يعني نصف الصوت)
+                audioPlayer?.play() // تشغيل الصوت
+            } catch {
+                print("حدث خطأ في تشغيل الصوت")
+            }
+        } else {
+            print("لم يتم العثور على ملف الصوت")
+        }
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // يتم تنفيذها عند انتقال التطبيق إلى الخلفية.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // يتم تنفيذها عند دخول التطبيق للخلفية.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // يتم تنفيذها عند عودة التطبيق إلى الواجهة.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // يتم تنفيذها عند إعادة تفعيل التطبيق.
     }
-
-
 }
-
